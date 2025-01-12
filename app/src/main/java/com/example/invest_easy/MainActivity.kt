a53ea078a1579c5746b7a1c82e9b22ed4d45050d
Bowie <marketing@letscode.com.br>
2025-01-11 22:34:11 -0300

    At3: criação de viewbindind

--- app/src/main/java/com/example/invest_easy/MainActivity.kt
+++ app/src/main/java/com/example/invest_easy/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.invest_easy
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.invest_easy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLimpar.setOnClickListener {
+            println("deu certo" + binding.edtAporte.text)
+            println("deu certo" + binding.edtMeses.text)
+            println("deu certo" + binding.edtJuros.text)
+
+        }
+
+
 
 
 
